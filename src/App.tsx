45d65006f585caf4966cc73eca6bf0a90a63d5ae
bracheopterix <bracheopterix@gmail.com>
2025-08-06 20:49:53 +0300

    first

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+function App(){
+
+
+    return (
+    <div className="mainContainer">
+
+        <div>Hello</div>
+
+    </div>
+    )
+}
+
+export default App;
---
+++ src/Notes/Notes.tsx
@@ -0,0 +1,14 @@
+
+function Notes(){
+
+
+    return (
+    <div className="componentContainer">
+
+        <div>Hello</div>
+
+    </div>
+    )
+}
+
+export default Notes;
