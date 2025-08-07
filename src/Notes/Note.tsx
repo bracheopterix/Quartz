9885542e39a784c3675975949b7e8a9c52d0b23f
bracheopterix <bracheopterix@gmail.com>
2025-08-07 13:14:22 +0300

    basic visuals,LocalStorage

---
+++ src/Notes/Note.tsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react';
+import styles from './Notes.module.scss'
+
+// type NoteType = {
+//     index:number,
+//     text:string,
+// }
+
+function Note() {
+    
+
+    
+
+    return (
+        <div className={styles.note}>
+
+            <div>Yesterday lalalalallalalal</div>
+
+            
+        </div>
+    )
+}
+
+export default Note;
--- src/Notes/Notes.tsx
+++ src/Notes/Notes.tsx
@@ -1,13 +1,56 @@
+import styles from './Notes.module.scss'
+import { useEffect, useState } from 'react';
+import Note from './Note'
 
-function Notes(){
 
+type Notes = string[];
+
+function Notes() {
+    const testItem: Notes = ["item1", "item2", "item3"];
+    localStorage.setItem('testItem', JSON.stringify(testItem));
+
+    const [Notes, setNotes] = useState<Notes | undefined>(undefined);
+
+    useEffect(() => {
+        const gotNotes = localStorage.getItem("testItem");
+        if (gotNotes) {
+            let parsedNotes: Notes;
+
+            try {
+                parsedNotes = JSON.parse(gotNotes);
+                setNotes(parsedNotes);
+                
+            } catch (error) {
+                console.log("Broke, assigning parsed Notes from LS", error);
+            }
+        }
+        else{
+            console.log("Failed to get Notes", gotNotes);
+        }
+
+    }, []);
 
     return (
-    <div className="componentContainer">
+        <div className={styles.componentContainer}>
 
-        <div>Hello</div>
+            <div className={styles.notesHolder}>
+            <h1>Notes</h1>
 
-    </div>
+            <Note />
+
+            {Notes?.map((element,index)=>(
+                <Note/>
+            ))}
+
+           
+            
+            </div>
+
+            
+            <button>Refresh</button>
+        </div>
+
+
     )
 }
 
