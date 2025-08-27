a98ca592f07eb529df57dd06ce1248a96881fc6e
bracheopterix <bracheopterix@gmail.com>
2025-08-27 15:41:21 +0300

    notes inlay, false generator, scroll function trigger

--- src/Notes/Notes.tsx
+++ src/Notes/Notes.tsx
@@ -3,21 +3,21 @@ import { useEffect, useState, useRef } from 'react';
 //Components
 import Note from './Note'
 //Types
-import type { NotesType, NoteType } from '../Types/Notes';
 
+/// TYPES ///
+// type NotesType = string[]
+type NoteType = string;
 
 
 type NotesParams = {
     serverAddress: string,
 }
 
-
-
-
+///  ----  ///
 
 function Notes(params: NotesParams) {
 
-    const [Notes, setNotes] = useState<NotesType | undefined>(undefined);
+    const [Notes, setNotes] = useState<NoteType[] | undefined>(undefined);
 
     useEffect(() => {
 
@@ -29,35 +29,75 @@ function Notes(params: NotesParams) {
 
     }, []);
 
+    /// notes slices generator ///
+    function* getNextPortionOfData(data: NoteType[] | undefined, slice: number) {
+        //^Hello, I am a generator!
+        let i = 0;
+        if (data) {
+            while (i < data.length) {
+                yield data.slice(i, i + slice);
+                i+=slice;
+            }
+            yield `Array's gone`
+        }
+        else{
+            yield "Can't yield anymore";
+        }
+
+
+    }
+
+    const testArray = ['1','2','3','4','5','6','7','8','9','10'];
+
+
+    const genNextPortionOfData = getNextPortionOfData(testArray, 4);
+
+
+    ///
+
     //// INFINITE SCROLL
 
-    const notesHolder = useRef<HTMLDivElement | null>(null);
+    const outerNotesContainer = useRef<HTMLDivElement | null>(null); // for now it is outer container
+    const innerNotesContainer = useRef<HTMLDivElement | null>(null); // for now it is all notes
+
+
 
 
     function handleScroll() {
-        console.log("height",document.documentElement.scrollHeight); // “how tall the whole scrollable page is.”
-        console.log("top",document.documentElement.scrollTop); // scroll value from top
-        console.log("window",window.innerHeight); // height of the visible window (viewport)
-
-
-
         // So the actual content height checks vs scrollHeight + viewportHeight (+1px for some browser's logic)=> 
         // because maximum scroll + viewport is the place where we need new data ASAP
         // so it is triggering a load function
         // What is important: it may and it will trigger itself multiply times before the data is loaded cause the condition is still true
         // So we need 'loading' state or some sort of a cycle with await and check.
 
-        console.log("scrollHeight:",notesHolder.current?.scrollHeight); // this should be inner container (msx losded hright)
-        console.log("Top: ",notesHolder.current?.scrollTop); // this should be inner container (scroll count)
-        console.log("window: ", window.innerHeight) // this should be top container ("viewport")
+        // if we are using all the window as our outer container, window.innerHeight will cover it's height.
 
 
+        // console.log("scrollHeight:", innerNotesContainer.current?.scrollHeight); // this should be inner container (msx losded hright)
+        // console.log("Top: ", outerNotesContainer.current?.scrollTop); // this should be inner container (scroll count)
+        // console.log("window: ", outerNotesContainer.current?.offsetHeight) // this should be top container ("viewport")
+
+        if (outerNotesContainer.current && innerNotesContainer.current && outerNotesContainer.current?.scrollTop + outerNotesContainer.current?.offsetHeight + 1 >= innerNotesContainer.current?.scrollHeight) {
+            // ^ we are equating scrolling position in the main (outer) container + it's "viewport" height with overall inner container height
+            // that mean that we are touched the bottom when the top-scroll number takes its maximum value (being one viewport heigher than the content height)
+
+
+            console.log(`I am loading new data`, genNextPortionOfData?.next().value);
+
+
+            // here we have a problem, cause the call for the content will fire multiple times before the content full load (as we still on the bottom of the content yet)
+            // so we should have some protection here
+
+        }
+        else {
+            console.log(`nah`);
+        }
 
     }
 
     useEffect(() => {
 
-        notesHolder.current?.addEventListener("scroll", handleScroll); // second parameter is the function that will be activated onscroll
+        outerNotesContainer.current?.addEventListener("scroll", handleScroll); // second parameter is the function that will be activated onscroll
 
 
     }, [])
@@ -67,26 +107,27 @@ function Notes(params: NotesParams) {
 
 
     return (
-        <div className={styles.componentContainer}>
+        <div className={styles.componentContainer} >
 
             <h1>Notes</h1>
+            <div className={styles.notesContainer} ref={outerNotesContainer}>
+                <div className={styles.notesHolder} ref={innerNotesContainer}>
+                    <div className={styles.scrollWise}>
 
-            <div className={styles.notesHolder} ref={notesHolder}>
-                <div className={styles.scrollWise}>
+                    </div>
+
+                    {Notes?.map((element: NoteType, index) => (
+                        <Note
+                            key={index}
+                            element={element}
+                        />
+                    ))}
+
+
+
+
 
                 </div>
-
-
-
-                {Notes?.map((element: NoteType, index) => (
-                    <Note
-                        key={index}
-                        element={element}
-                    />
-                ))}
-
-
-
             </div>
 
             <div className={styles.buttonHolder}>
